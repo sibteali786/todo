1eb8a43ebcf2f226c87620b1e44bfb8a04d1ca13
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-10-19 15:40:22 +0500

    implement interface for string and fixed failing test

--- cmd/main.go
+++ cmd/main.go
@@ -38,12 +38,7 @@ func main() {
 
 	switch {
 	case *list:
-		// List current to-do items
-		for _, item := range *l {
-			if !item.Done {
-				fmt.Println(item.Task)
-			}
-		}
+		fmt.Print(l)
 	case *complete > 0:
 		// Complete the given item
 		if err := l.Complete(*complete); err != nil {
