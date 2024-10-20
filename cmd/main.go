54e6109f1c01bac2bc95893ad7bf43c6983d2444
Syed Sibteali Baqar <sibteali786@gmail.com>
2024-10-20 16:02:38 +0500

    added getTask to replace simple Add fucntion from todo list

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	"mygoprograms.com/todo"
 )
@@ -25,7 +28,7 @@ func main() {
 	}
 
 	// Define command-line flags
-	task := flag.String("task", "", "Task to be included in the ToDo List")
+	add := flag.Bool("add", false, "Add task to the ToDo List")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item to be completed")
 
@@ -54,9 +57,15 @@ func main() {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-	case *task != "":
+	case *add:
 		// Add a new task
-		l.Add(*task)
+		t, err := getTask(os.Stdin, flag.Args()...)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		l.Add(t)
+		// Save the new list
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
@@ -66,4 +75,21 @@ func main() {
 		fmt.Fprintln(os.Stderr, "Invalid option")
 		os.Exit(1)
 	}
+
+}
+func getTask(r io.Reader, args ...string) (string, error) {
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+	s := bufio.NewScanner(r)
+	s.Scan()
+
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+
+	if len(s.Text()) == 0 {
+		return "", fmt.Errorf("Task cannot be blank")
+	}
+	return s.Text(), nil
 }
